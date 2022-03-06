c49f5d24d2449a4265319766b7ae37b44a0b06fe
troy adianto <troy.adianto@gmail.com>
2022-03-06 15:01:58 +0800

    update model deploy

---
+++ fraud-api-deploy.sh
@@ -0,0 +1,18 @@
+NAMESPACE=fraud-detector
+
+oc new-project $NAMESPACE
+
+oc new-app https://github.com/troy-adianto/python-ml-deploy-demo.git -n $NAMESPACE \
+--context-dir=model-deploy \
+--strategy=source \
+--name=fraud-detector-api
+
+oc expose service fraud-detector-api -n $NAMESPACE
+
+ROUTE_URL="http://"$(oc get route fraud-detector-api -n $NAMESPACE -o jsonpath='{.spec.host}')
+
+oc apply -f fraud-detector-mock-service-monitor.yaml -n $NAMESPACE
+
+echo "\n\nFraud detector api succesfully deployed please use the following link to test after the build is completed\n\n"   $ROUTE_URL"\n\n"
+
+echo "use 'predict' endpoint to get detection result"
