e2f12fc7486cba7a5df1692c3b240e17ca75a34c
troy adianto <troy.adianto@gmail.com>
2022-03-06 14:26:35 +0800

    update app kubefiles deployment

--- deploy/app.py
+++ deploy/app.py
@@ -6,21 +6,29 @@ import random
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app, group_by='endpoint')
+percentage=1
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+@app.route('/setFraudPercentage')
+def setFraudPercentage():
+	percentage= request.args.key("value")
+    
+    return "Fraud percentage changed to"+percentage
+
 @app.route('/predict')
 @metrics.counter('predict', 'Number of prediction',
          labels={'result': lambda: request.result})
 def predict():
+	top = 100/percentage
 	request.result = 'invalid'
-	number = random.randint(0,100)
+	number = random.randint(0,top)
 	if number == 0:
 	 request.result = 'fraud'
 	else:
-	  request.result = 'not fraud'
+	  request.result = 'normal'
 	return request.result
 
 if __name__ == '__main__':
--- fraud-monitoring-deploy.sh
+++ fraud-monitoring-deploy.sh
@@ -3,3 +3,18 @@ NAMESPACE="fraud-monitor"
 oc new-project $NAMESPACE
 
 sed -e "s|NAMESPACE|$NAMESPACE|g" ./kubefiles/grafana-deployment.yaml | oc apply -n $NAMESPACE -f -
+
+oc adm policy add-cluster-role-to-user cluster-monitoring-view -z grafana-serviceaccount -n $NAMESPACE
+
+TOKEN=$(oc serviceaccounts get-token grafana-serviceaccount -n $NAMESPACE)
+
+sed -e "s|TOKEN|$TOKEN|g" ./kubefiles/grafana-datasource.yaml | oc apply -n $NAMESPACE -f -
+
+oc apply -n $NAMESPACE -f ./kubefiles/fraud-detection-dashboard.yaml
+
+ADMIN_USER=$(oc get secret grafana-admin-credentials -n $NAMESPACE --template={{.data.GF_SECURITY_ADMIN_USER}} | base64 -d)
+ADMIN_PASSWORD=$(oc get secret grafana-admin-credentials -n $NAMESPACE --template={{.data.GF_SECURITY_ADMIN_PASSWORD}} | base64 -d)
+
+GRAFANA_URL="https://"$(oc get route grafana-route -n $NAMESPACE -o jsonpath='{.spec.host}')
+
+echo "\n\nGrafana has been successfully deployed, you can use the following url to access\n\n"    $GRAFANA_URL  "\n\n. Admin user : "$ADMIN_USER "\n. Password : "$ADMIN_PASSWORD"\n\n"
